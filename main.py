1996ad3e42f7e9364471a8f8d2a72cc633f8f512
Charith Amarasinghe <charithamarasinghe024@gmail.com>
2021-03-04 22:36:00 +0530

    adding files

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/home/projects/webScrapingDamian/scraperEnv/bin/python3
+
+import time
+import csv
+import json
+import logging
+import configparser
+import os
+import sys
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(1, base_path)
+
+import scraper
+
+config = configparser.ConfigParser()
+config.read(base_path + "/config.ini")
+
+logging.basicConfig(filename=config["MAIN"]["log_file"], filemode='w',
+                    format="%(asctime)s %(levelname)s: %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def main():
+    logger.info("job started")
+
+    location_val = "losangeles"
+    distance_val = "50"
+    postal_val = 90069
+    auto_make_model_val = "BMW"
+    min_auto_year_val = "2013"
+    max_auto_year_val = "2018"
+    max_auto_miles_val = "80000"
+    auto_title_status_val = "1"
+
+    c_scraper = scraper.CraiglistScraper(location=location_val, distance=distance_val, postal=postal_val,
+                                         auto_make_model=auto_make_model_val, min_auto_year=min_auto_year_val,
+                                         max_auto_year=max_auto_year_val, max_auto_miles=max_auto_miles_val,
+                                         auto_title_status=auto_title_status_val)
+    data = c_scraper.extract_post_data()
+
+    return data
+
+
+if __name__ == "__main__":
+    main()
---
+++ scraper.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+from bs4 import BeautifulSoup
+import os
+import configparser
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+
+config = configparser.ConfigParser()
+config.read(base_path + "/config.ini")
+
+web_driver_file = base_path + config["MAIN"]["web_driver_file"]
+
+
+def generate_url(location, distance, postal, auto_make_model, min_auto_year, max_auto_year, max_auto_miles,
+                 auto_title_status):
+    return f"https://{location}.craigslist.org/search/cta?purveyor-input=all&search_distance={distance}&" \
+           f"postal={postal}&auto_make_model={auto_make_model}&min_auto_year={min_auto_year}&max_auto_year" \
+           f"={max_auto_year}&max_auto_miles={max_auto_miles}&auto_title_status={auto_title_status}"
+
+
+def get_sel_page(url: str) -> BeautifulSoup:
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    options.add_argument('--disable-infobars')
+    options.add_argument('-disable-notifications')
+    options.add_argument('--no-sandbox')
+
+    driver = webdriver.Chrome(web_driver_file, options=options)
+    driver.get(url)
+    try:
+        delay = 5
+        wait = WebDriverWait(driver, delay)
+        wait.until(ec.presence_of_element_located((By.ID, "searchform")))
+        print("Page is ready")
+    except TimeoutException:
+        print("Loading took too much time")
+
+    page = driver.page_source
+    page_soup = BeautifulSoup(page, 'html.parser')
+    driver.stop_client()
+    driver.close()
+    driver.quit()
+
+    return page_soup
+
+
+def get_chrome_options():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    options.add_argument('--disable-infobars')
+    options.add_argument('-disable-notifications')
+    options.add_argument('--no-sandbox')
+    return options
+
+
+class CraiglistScraper(object):
+    def __init__(self, location, distance, postal, auto_make_model, min_auto_year, max_auto_year, max_auto_miles,
+                 auto_title_status):
+        self.location = location
+        self.distance = distance
+        self.postal = postal
+        self.auto_make_model = auto_make_model
+        self.min_auto_year = min_auto_year
+        self.max_auto_year = max_auto_year
+        self.max_auto_miles = max_auto_miles
+        self.auto_title_status = auto_title_status
+
+        self.url = f"https://{location}.craigslist.org/search/cta?purveyor-input=all&search_distance={distance}&" \
+                   f"postal={postal}&auto_make_model={auto_make_model}&min_auto_year={min_auto_year}&max_auto_year" \
+                   f"={max_auto_year}&max_auto_miles={max_auto_miles}&auto_title_status={auto_title_status}"
+
+    def extract_post_data(self):
+        page_soup = get_sel_page(self.url)
+        page_rows = page_soup.find("ul", {"class": "rows"})
+        page_row_list = page_rows.findAll("li", {"class": "result-row"})
+
+        all_post_data = []
+        for post in page_row_list:
+            try:
+                post_data = dict()
+                post_data["price"] = post.find("span", {"class": "result-price"}).text
+
+                basic_info = post.find("div", {"class": "result-info"})
+                post_data["date_time"] = basic_info.find("time", {"class": "result-date"})['datetime']
+                post_data["post_url"] = basic_info.find("a", {"class": "result-title hdrlnk"})['href']
+                post_data["post_title"] = basic_info.find("a", {"class": "result-title hdrlnk"}).text
+                all_post_data.append(post_data)
+            except Exception as error:
+                print(str(error))
+
+        return all_post_data
+
+
+location_val = "losangeles"
+distance_val = "50"
+postal_val = 90069
+auto_make_model_val = "BMW"
+min_auto_year_val = "2013"
+max_auto_year_val = "2018"
+max_auto_miles_val = "80000"
+auto_title_status_val = "1"
+
+scraper = CraiglistScraper(location=location_val, distance=distance_val, postal=postal_val,
+                           auto_make_model=auto_make_model_val, min_auto_year=min_auto_year_val,
+                           max_auto_year=max_auto_year_val, max_auto_miles=max_auto_miles_val,
+                           auto_title_status=auto_title_status_val)
+data = scraper.extract_post_data()
+
